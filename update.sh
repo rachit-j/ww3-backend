e859e5ba101556879fe3b6c48e57380148698992
rachit-j <rjaiswal.sd.77@gmail.com>
2023-11-21 10:21:16 -0800

    Made script pretty

--- update.sh
+++ update.sh
@@ -1,8 +1,10 @@
 #!/bin/bash
 
 echo "Updating and redeploying the ww3-backend. If no changes are shown, please run: service nginx restart"
+echo "----------------------------------------------------------------------------------------------------------------"
 cd /root/ww3-backend/
 docker-compose down
 git pull .
 docker-compose up -d --build
+echo "----------------------------------------------------------------------------------------------------------------"
 echo "Update Complete"
