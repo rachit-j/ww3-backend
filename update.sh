39b7a7a609416dc724e88b180066b783661f78ad
rachit-j <rjaiswal.sd.77@gmail.com>
2023-11-21 11:27:15 -0800

    Final modifications to the update script

--- update.sh
+++ update.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+# WARNING: PLEASE DO NOT MODIFY THIS SCRIPT. IF YOU DO, YOU MUST RUN THE COMMANDS MANUALLY TO REPULL THE GIT REPOSITORY SO THAT THE SCRIPT DOES NOT CHANGE AS IT EXECUTES. MAKE SURE TO SEND AN ALERT IF THIS HAS CHANGED!
+
 echo "Updating and redeploying the ww3-backend. If no changes are shown, please run: service nginx restart"
 echo "----------------------------------------------------------------------------------------------------------------"
 cd /root/ww3-backend/
@@ -7,4 +9,4 @@ docker-compose down
 git pull .
 docker-compose up -d --build
 echo "----------------------------------------------------------------------------------------------------------------"
-echo "Update Complete"
+echo "Update Complete. If no changes are shown, please run: service nginx restart. To check the server status, please run: service nginx status"
