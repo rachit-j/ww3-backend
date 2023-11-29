3a3bebefea7f829d1c7bd911ae654715d9bb4c53
tanishapatil1234 <tanishapatil07@gmail.com>
2023-11-29 12:41:34 -0800

    ApiController Cards

--- src/main/java/com/nighthawk/spring_portfolio/mvc/card/CardApiController.java
+++ src/main/java/com/nighthawk/spring_portfolio/mvc/card/CardApiController.java
@@ -1,5 +1,24 @@
 package com.nighthawk.spring_portfolio.mvc.card;
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping(path = "/api/card")
 public class CardApiController {
+   
+    @Autowired
+    private CardJpaRepository repository; 
+
+    @GetMapping("/")
+    public ResponseEntity<List<Card>> getCard() {
+        return new ResponseEntity<>(repository.findAll(), HttpStatus.OK);
+    }
     
 }
