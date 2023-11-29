e667a33dc762f45c5ae6d5609235302aa4feb0f6
tanishapatil1234 <tanishapatil07@gmail.com>
2023-11-29 12:41:48 -0800

    JPA Repository Cards

---
+++ src/main/java/com/nighthawk/spring_portfolio/mvc/card/CardJpaRepository.java
@@ -0,0 +1,11 @@
+package com.nighthawk.spring_portfolio.mvc.card;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface CardJpaRepository extends  JpaRepository<Card, Long>{
+    void save(String Card);
+}
+    
+
