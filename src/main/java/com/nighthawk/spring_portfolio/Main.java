a85609b0d9ea497f260e6bc590581b439a1f8364
rachit-j <rjaiswal.sd.77@gmail.com>
2023-12-07 00:00:56 -0800

    added cors?

--- src/main/java/com/nighthawk/spring_portfolio/Main.java
+++ src/main/java/com/nighthawk/spring_portfolio/Main.java
@@ -2,14 +2,27 @@ package com.nighthawk.spring_portfolio;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
-// @SpringBootApplication annotation is the key to building web applications with Java https://spring.io/projects/spring-boot
 @SpringBootApplication
 public class Main {
 
-    // Starts a spring application as a stand-alone application from the main method
     public static void main(String[] args) {
         SpringApplication.run(Main.class, args);
     }
 
+    // Add this method to configure CORS
+    @Bean
+    public WebMvcConfigurer corsConfigurer() {
+        return new WebMvcConfigurer() {
+            @Override
+            public void addCorsMappings(CorsRegistry registry) {
+                // Adjust this to match the domain from which you expect to receive requests
+                // Use "*" to allow all origins (not recommended for production)
+                registry.addMapping("/**").allowedOrigins("https://rachit-j.github.io");
+            }
+        };
+    }
 }
